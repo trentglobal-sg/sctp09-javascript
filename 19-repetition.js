cbede88694f956a70dca88d7a931d246ad3dbd9f
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2025-11-18 05:32:37 +0000

    Add while loop example

---
+++ 19-repetition.js
@@ -0,0 +1,24 @@
+// REPETITION
+// Doing something over and over again
+
+// A while loop allows us to repeat lines of code in a block
+let count = 0;
+while (count < 10 ) {
+    console.log("I will remember to bring my homework");
+    count = count + 1;
+}
+
+// Show the numbers from 1 to 5
+let num = 1;
+while (num < 5) {
+    console.log(num);
+    num = num + 1;
+}
+console.log("Finished");
+
+let num = 1;
+while (num < 6) {
+    console.log(num);
+    num = num + 1;
+}
+console.log("Finished");
