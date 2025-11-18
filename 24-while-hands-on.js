262915d6615e0fd9b8fa45fbf95cf06ce535ad13
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2025-11-18 06:54:30 +0000

    While hands ons added

---
+++ 24-while-hands-on.js
@@ -0,0 +1,16 @@
+// Roller Coaster Entry
+// Ask the user to enter the age of all the visitors one by one
+// Make sure age is greater than 0 and is lesser than 80
+// For adult,  age greater or equal to 18, and are charged $10
+// For children, each are charged $6
+// If the user enters -99, then stop asking and show the final charge 
+//
+// SAMPLE OUTPUT
+// Enter the age: 5
+// Enter the age: -6
+// Invalid age. Try again
+// Enter the age: 42
+// Enter the age: -99
+// One adult
+// One children
+// Total: $16
